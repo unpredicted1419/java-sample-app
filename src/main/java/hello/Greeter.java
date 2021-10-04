89bbedddafc70e1fe5f29e76c22d0116633618f6
crazy4devops <84403055+crazy4devops@users.noreply.github.com>
2021-10-04 12:43:20 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -4,4 +4,15 @@ public class Greeter {
     public String sayHello() {
         return "Dummy Devops";
     }
+     public String sayHello() {
+        return "Dummy Devops";
+    }
+     public String sayHello() {
+        return "Dummy Devops";
+    }
+     public String sayHello() {
+        return "Dummy Devops";
+    } public String sayHello() {
+        return "Dummy Devops";
+    }
 }
